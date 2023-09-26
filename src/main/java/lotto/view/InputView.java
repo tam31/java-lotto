f840bfc8409c8586dd90c9e6cb41593e31107a1d
tam31 <wxogud3377@naver.com>
2023-09-26 21:40:28 +0900

    Feat(InputView): 출력메시지 설정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,8 @@
 package lotto.view;
 
 public class InputView {
+    private static final String ENTER_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String Lotto_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONES_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
 }
