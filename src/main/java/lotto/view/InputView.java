8ca3c895f0856503444b9545693d010e2bfa24e6
tam31 <wxogud3377@naver.com>
2023-09-26 21:42:15 +0900

    Feat(InputView): 로또 구입금액 출력메시지 기능 Docs: 로또 금액 입력 완료 체크

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,4 +5,8 @@ public class InputView {
     private static final String Lotto_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONES_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
+    public static void inputMoney(){
+        System.out.println(ENTER_MONEY_MESSAGE);
+    }
+
 }
