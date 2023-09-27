794cc92abefd31dd05e8954032e254abbe7b70fa
tam31 <wxogud3377@naver.com>
2023-09-27 13:09:56 +0900

    Feat(LottoNumber): 당첨번호의 데이터유효성검사

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.Lotto;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoNumber {
@@ -12,5 +15,20 @@ public class LottoNumber {
 
     public List<Integer> splitNumber(String lottoNumber){
         String[] numbers = lottoNumber.split(",");
+        List<Integer> lottoNumberList = addLottoNumber(numbers);
+        Lotto lotto = new Lotto(lottoNumberList);
+        return lotto.getNumbers();
+    }
+
+    private List<Integer> addLottoNumber(String[] numbers) {
+        List<Integer> lottoNumberList = new ArrayList<>();
+        for(String number: numbers){
+            lottoNumberList.add(changeNumber(number));
+        }
+        return lottoNumberList;
+    }
+
+    private int changeNumber(String number) {
+        return Integer.parseInt(number);
     }
 }
