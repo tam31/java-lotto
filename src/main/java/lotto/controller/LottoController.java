78d31051b9f1998050212a4a18c6d8dd94b2a2ed
tam31 <wxogud3377@naver.com>
2023-09-27 13:00:35 +0900

    Feat(LottoNumber): 로또번호의 도메인 만들기

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.LottoNumber;
 import lotto.domain.UserNumbers;
 import lotto.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
@@ -8,10 +9,13 @@ import lotto.view.OutputView;
 public class LottoController {
 
     private static UserNumbers user;
+    private static LottoNumber lotto;
     public void startGame(){
         InputView.inputMoney();
         user = new UserNumbers(Console.readLine());
         OutputView.lottoCount(user.getMoney(), user.getUserLottoMap());
+        InputView.lottoNumber();
+        lotto = new LottoNumber(Console.readLine());
         System.out.println(user.getUserLottoMap());
     }
 }
---
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoNumber {
+    private List<Integer> lottoNumber;
+    private Integer bonus;
+
+    public LottoNumber(String lottoNumber) {
+        this.lottoNumber = splitNumber(lottoNumber);
+    }
+}
