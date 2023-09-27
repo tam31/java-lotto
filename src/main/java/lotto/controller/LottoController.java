9127fe589af3427ce1eefda1d0ae3518bef8bd51
tam31 <wxogud3377@naver.com>
2023-09-27 11:43:13 +0900

    Feat(OutputView): 구매한 로또 수량 출력메시지 Docs: 로또 구매수량 메시지 완료체크

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.domain.UserNumbers;
 import lotto.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
+import lotto.view.OutputView;
 
 public class LottoController {
 
@@ -10,6 +11,7 @@ public class LottoController {
     public void startGame(){
         InputView.inputMoney();
         user = new UserNumbers(Console.readLine());
+        OutputView.lottoCount(user.getMoney(), user.getUserLottoMap());
         System.out.println(user.getUserLottoMap());
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,15 @@
 package lotto.view;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class OutputView {
+    private final static int DIVID = 1000;
+    private final static String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
+
+    public static void lottoCount(int money,HashMap<List<Integer>, Integer> lottoNumbers){
+        System.out.println(money/DIVID+LOTTO_COUNT_MESSAGE);
+    }
+
+
 }
