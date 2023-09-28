566bffacf775a4bb5308725e717b3ac8864b18b9
tam31 <wxogud3377@naver.com>
2023-09-28 22:33:54 +0900

    Feat(DataValidation): 당첨번호의 숫자확인 및 형변환 데이터 유효성 검사

--- src/main/java/lotto/validation/DataValidation.java
+++ src/main/java/lotto/validation/DataValidation.java
@@ -1,23 +1,50 @@
 package lotto.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataValidation {
     private static int unitMoney = 1000;
 
-    public int isCheckMoney(String money){
+    public int isCheckNumber(String number){
         try{
-            int result = changeMoney(money);
+            int result = changeNumber(number);
             return result;
         }catch (NumberFormatException n){
-            throw new IllegalArgumentException("[ERROR] 금액을 입력하세요");
+            throw new IllegalArgumentException("[ERROR] 숫자을 입력하세요");
         }
     }
 
-    public int changeMoney(String money){
-        return Integer.parseInt(money);
+    public int changeNumber(String number){
+        return Integer.parseInt(number);
     }
     public void checkMoney(int money){
         if(money%unitMoney!=0){
             throw new IllegalArgumentException(String.format("[ERROR]%d원 단위로 나눌수 없습니다.",unitMoney));
         }
+    }
+
+    public List<Integer> isCheckNumbers(String numbers) {
+        return splitNumbers(numbers);
+    }
+
+    private List<Integer> splitNumbers(String numbers) {
+        try{
+            String[] strNumbers = numbers.split(",");
+            List<Integer> number = changeNumbers(strNumbers);
+            return number;
+        }catch (ArrayIndexOutOfBoundsException a){
+            throw new IllegalArgumentException("[ERROR] 6가지의 숫자를 ','와 함께 써주세요");
+        }catch (Exception e){
+            throw new IllegalArgumentException("[ERROR] 6가지의 숫자를 ','와 함께 써주세요");
+        }
+    }
+
+    private List<Integer> changeNumbers(String[] strNumbers) {
+        List<Integer> number = new ArrayList<>();
+        for(String checkNumber:strNumbers){
+            number.add(isCheckNumber(checkNumber));
+        }
+        return number;
     }
 }
