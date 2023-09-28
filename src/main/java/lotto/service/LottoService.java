73c6b96fe6b6f8a311f21a168282697211b8173e
tam31 <wxogud3377@naver.com>
2023-09-28 22:03:07 +0900

    Feat(LottoService): 구매한 로또 수만큼의 로또번호 뽑기

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,10 +1,33 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.Lotto;
 import lotto.validation.DataValidation;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class LottoService {
+
+    private static final int START = 1;
+    private static final int END = 45;
+    private static final int COUNT = 6;
+    private static final int DIVIDE = 1000;
+    private static HashMap<List<Integer>,Integer> lottoList = new HashMap<>();
     DataValidation dataValidation = new DataValidation();
     public int checkMoney(String money){
         return dataValidation.isCheckMoney(money);
+    }
+    public HashMap<List<Integer>,Integer> pullLottoNumber(int money){
+        for(int i=0; i<money/DIVIDE; i++){
+            List<Integer> numbers = getNumbers();
+            Lotto lotto = new Lotto(numbers);
+            lottoList.put(lotto.getNumbers(),i);
+        }
+        return lottoList;
+    }
+
+    private List<Integer> getNumbers() {
+        return Randoms.pickUniqueNumbersInRange(START, END, COUNT);
     }
 }
