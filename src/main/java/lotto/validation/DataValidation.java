38c3689770088fd0d6518d31a8cb89bf9cc6d2ae
tam31 <wxogud3377@naver.com>
2023-09-28 17:25:41 +0900

    Feat(validation): 입력받은 금액의 데이터유효성 검사

---
+++ src/main/java/lotto/validation/DataValidation.java
@@ -0,0 +1,23 @@
+package lotto.validation;
+
+public class DataValidation {
+    private static int unitMoney = 1000;
+
+    public static int isCheckMoney(String money){
+        try{
+            int result = changeMoney(money);
+            return result;
+        }catch (NumberFormatException n){
+            throw new IllegalArgumentException("[ERROR] 금액을 입력하세요");
+        }
+    }
+
+    public static int changeMoney(String money){
+        return Integer.parseInt(money);
+    }
+    public static void checkMoney(int money){
+        if(money%unitMoney!=0){
+            throw new IllegalArgumentException(String.format("[ERROR]%d원 단위로 나눌수 없습니다.",unitMoney));
+        }
+    }
+}
