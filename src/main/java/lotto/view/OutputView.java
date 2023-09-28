0e42862185b39606cfa235e488fbe45a4a8148ec
tam31 <wxogud3377@naver.com>
2023-09-28 14:01:18 +0900

    Feat(OutputView): 당첨번호에 따른 당첨개수 출력메시지 Docs: 당첨개수에 따른 통계메시지 완료체크

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.Rank;
+
 import java.util.HashMap;
 import java.util.List;
 
@@ -7,6 +9,7 @@ public class OutputView {
     private final static int DIVID = 1000;
     private final static String LOTTO_COUNT_MESSAGE = "개를 구매했습니다.";
     private final static String SUMMARY_MESSAGE = "당첨통계.";
+    private final static String LOTTO_RESULT_COUNT_MESSAGE = "%d개 일치 (%s원) - %d개\n";
 
     public static void lottoCount(int money,HashMap<List<Integer>, Integer> lottoNumbers){
         System.out.println(money/DIVID+LOTTO_COUNT_MESSAGE);
@@ -19,9 +22,17 @@ public class OutputView {
         }
     }
 
-    public static void summaryLotto(){
+    public static void summaryLotto(HashMap<Integer, Integer> lottoCount){
         System.out.println(SUMMARY_MESSAGE);
         System.out.println("---");
+        winLottoCount(lottoCount);
+    }
+
+    public static void winLottoCount(HashMap<Integer, Integer> lottoCount) {
+        for(Rank rank:Rank.values()){
+            System.out.printf(LOTTO_RESULT_COUNT_MESSAGE,rank.getCount(),rank.getViewMoney(),lottoCount.get(rank.getCount()));
+        }
+
     }
 
 }
