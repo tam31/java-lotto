bc0b282737924a56c760d6f144c61a2b66779e5e
tam31 <wxogud3377@naver.com>
2023-09-28 13:50:43 +0900

    Feat(Rank): 당첨금출력문을 위한 상수 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,18 +1,20 @@
 package lotto.domain;
 
 public enum Rank {
-    Three(3,5000l),
-    Four(4,50000l),
-    Five(5, 1500000l),
-    Six(6, 30000000l),
-    Seven(7, 2000000000l);
+    Three(3,5000l,"5,000"),
+    Four(4,50000l,"50,000"),
+    Five(5, 1500000l,"1,500,000"),
+    Six(6, 30000000l,"30,000,000"),
+    Seven(7, 2000000000l,"2,000,000,000");
 
     final private int count;
     final private Long money;
+    final private String viewMoney;
 
-    Rank(int count, Long money) {
+    Rank(int count, Long money,String viewMoney) {
         this.count = count;
         this.money = money;
+        this.viewMoney = viewMoney;
     }
 
     public int getCount() {
@@ -23,4 +25,7 @@ public enum Rank {
         return money;
     }
 
+    public String getViewMoney() {
+        return viewMoney;
+    }
 }
