c8d86e1ff1446ab4d07b07446313104303a3d51f
tam31 <wxogud3377@naver.com>
2023-09-28 22:52:48 +0900

    Refactor(LottoNumber): 당첨번호와 보너스번호의 도메인 수정 및 삭제

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -9,8 +9,9 @@ public class LottoNumber {
     private List<Integer> lottoNumber;
     private Integer bonus;
 
-    public LottoNumber(String lottoNumber) {
-        this.lottoNumber = splitNumber(lottoNumber);
+    public LottoNumber(List<Integer> lottoNumber, Integer bonus) {
+        this.lottoNumber = lottoNumber;
+        this.bonus = bonus;
     }
 
     public List<Integer> getLottoNumber() {
@@ -21,38 +22,5 @@ public class LottoNumber {
         return bonus;
     }
 
-    public void putBonus(String number) {
-        this.bonus = checkBonusNumber(number);
-    }
 
-    public int checkBonusNumber(String number){
-        int bonusNumber = changeNumber(number);
-        isNotDuplicationNumber(bonusNumber);
-        return bonusNumber;
-    }
-
-    private void isNotDuplicationNumber(int bonusNumber) {
-        if(getLottoNumber().contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 당첨번호와 보너스 번호가 중복입니다.");
-        }
-    }
-
-    public List<Integer> splitNumber(String lottoNumber){
-        String[] numbers = lottoNumber.split(",");
-        List<Integer> lottoNumberList = addLottoNumber(numbers);
-        Lotto lotto = new Lotto(lottoNumberList);
-        return lotto.getNumbers();
-    }
-
-    private List<Integer> addLottoNumber(String[] numbers) {
-        List<Integer> lottoNumberList = new ArrayList<>();
-        for(String number: numbers){
-            lottoNumberList.add(changeNumber(number));
-        }
-        return lottoNumberList;
-    }
-
-    private int changeNumber(String number) {
-        return Integer.parseInt(number);
-    }
 }
