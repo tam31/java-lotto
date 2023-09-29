4a1c327d6d4092a5c5dd521767524b0f2ede4b36
tam31 <wxogud3377@naver.com>
2023-09-29 15:55:06 +0900

    Refactor(LottoController): 메서드 길이의 요구사항 충족

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -18,27 +18,40 @@ public class LottoController {
     private static LottoService service = new LottoService();
     public void startGame(){
         try {
-            InputView.inputMoney();
             user = new UserNumbers();
-            user.inputMoney(service.checkMoney(Console.readLine()));
-            user.pullLottoNumbers(service.pullLottoNumber(user.getMoney()));
-            OutputView.lottoCount(user.getMoney(), user.getPullLottoNumbers());
+            buyLottoAndNumber();
 
-            InputView.lottoNumber();
-            List<Integer> isLottoNumbers = service.inputLottoNumber(Console.readLine());
-            InputView.bonesNumber();
-            int bonusNumber = service.inputBonuse(Console.readLine(), isLottoNumbers);
-            lotto = new LottoNumber(isLottoNumbers, bonusNumber);
+            getLottoNumber();
 
-            HashMap<Rank, Integer> rankLottoCount = service.resultLottoCount(user.getPullLottoNumbers(), lotto.getLottoNumber(), lotto.getBonus(), user.getLottoCount());
-            user.isLottoCount(rankLottoCount);
-            OutputView.winLottoCount(user.getLottoCount());
-
-            user.addBenefitMoney(service.sumLottoBenefit(rankLottoCount, user.getMoney()));
-            OutputView.allBenefit(user.getBenefitMoney());
+            getLottoCount();
+            getBenefitPercent();
         }catch (IllegalArgumentException e){
 
         }
     }
 
+    private void buyLottoAndNumber() {
+        InputView.inputMoney();
+        user.inputMoney(service.checkMoney(Console.readLine()));
+        user.pullLottoNumbers(service.pullLottoNumber(user.getMoney()));
+        OutputView.lottoCount(user.getMoney(), user.getPullLottoNumbers());
+    }
+
+    private void getLottoNumber() {
+        InputView.lottoNumber();
+        List<Integer> isLottoNumbers = service.inputLottoNumber(Console.readLine());
+        InputView.bonesNumber();
+        int bonusNumber = service.inputBonuse(Console.readLine(), isLottoNumbers);
+        lotto = new LottoNumber(isLottoNumbers, bonusNumber);
+    }
+
+    private void getLottoCount(){
+        HashMap<Rank, Integer> rankLottoCount = service.resultLottoCount(user.getPullLottoNumbers(), lotto.getLottoNumber(), lotto.getBonus(), user.getLottoCount());
+        user.isLottoCount(rankLottoCount);
+        OutputView.summaryLotto(user.getLottoCount());
+    }
+    private void getBenefitPercent() {
+        user.addBenefitMoney(service.sumLottoBenefit(user.getLottoCount(), user.getMoney()));
+        OutputView.allBenefit(user.getBenefitMoney());
+    }
 }
